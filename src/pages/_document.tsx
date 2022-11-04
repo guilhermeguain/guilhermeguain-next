e8c0b5d639fe4a5d9fa9fdc7d035c5370b223b86
Guilherme Guain <guilherme.guain@gmail.com>
2022-11-04 16:50:25 -0300

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,13 @@ function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap"
           rel="stylesheet"
         />
+
+        <meta name="theme-color" content="gray.800" />
+        <meta
+          name="robots"
+          content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
+        />
+        <link rel="icon" type="image/png" href="/images/favicon.png" />
       </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
