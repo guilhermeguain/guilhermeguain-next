c09126a120fda2a664188990de3716f7073bcb77
Guilherme Guain <guilherme.guain@gmail.com>
2022-11-12 18:02:58 -0300

    test

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['en-US', 'pt-BR'],
     defaultLocale: 'en-US',
     localeDetection: false,
+    localePath: path.resolve('./public/static/locales'),
     domains: [
       {
         domain: process.env.NEXT_PUBLIC_EN_URL || 'https://www.guilhermeguain.com',
--- next.config.js
+++ next.config.js
@@ -1,24 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { i18n } = require('./next-i18next.config');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
-    locales: ['en-US', 'pt-BR'],
-    defaultLocale: 'en-US',
-    localeDetection: false,
-    domains: [
-      {
-        domain: process.env.NEXT_PUBLIC_EN_URL || 'https://www.guilhermeguain.com',
-        defaultLocale: 'en-US',
-        locales: ['en-US']
-      },
-      {
-        domain: process.env.NEXT_PUBLIC_PT_URL || 'https://www.guilhermeguain.com.br',
-        defaultLocale: 'pt-BR',
-        locales: ['pt-BR']
-      }
-    ]
-  }
+  i18n
 };
 
 module.exports = nextConfig;
