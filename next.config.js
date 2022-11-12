192b17be4591a696f4e257f07eb9ca261effb357
Guilherme Guain <guilherme.guain@gmail.com>
2022-11-12 17:56:00 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { i18n } = require('./next-i18next.config');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n
+  i18n: {
+    locales: ['en-US', 'pt-BR'],
+    defaultLocale: 'en-US',
+    localeDetection: false,
+    domains: [
+      {
+        domain: process.env.NEXT_PUBLIC_EN_URL || 'https://www.guilhermeguain.com',
+        defaultLocale: 'en-US',
+        locales: ['en-US']
+      },
+      {
+        domain: process.env.NEXT_PUBLIC_PT_URL || 'https://www.guilhermeguain.com.br',
+        defaultLocale: 'pt-BR',
+        locales: ['pt-BR']
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
