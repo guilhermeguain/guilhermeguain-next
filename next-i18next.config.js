d7db559fd7c983b0e4ab4648cc936e161bfdb5d5
Guilherme Guain <guilherme.guain@gmail.com>
2022-11-12 17:37:20 -0300

    i18n test

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,19 +1,20 @@
-module.exports = {
-  i18n: {
-    locales: ['en-US', 'pt-BR'],
-    defaultLocale: 'en-US',
-    localeDetection: false,
-    domains: [
-      {
-        domain: process.env.NEXT_PUBLIC_EN_URL || 'https://www.guilhermeguain.com',
-        defaultLocale: 'en-US',
-        locales: ['en-US']
-      },
-      {
-        domain: process.env.NEXT_PUBLIC_PT_URL || 'https://www.guilhermeguain.com.br',
-        defaultLocale: 'pt-BR',
-        locales: ['pt-BR']
-      }
-    ]
-  }
+import { resolve } from 'path';
+
+export const i18n = {
+  locales: ['en-US', 'pt-BR'],
+  defaultLocale: 'en-US',
+  localeDetection: false,
+  localePath: resolve('./public/locales'),
+  domains: [
+    {
+      domain: process.env.NEXT_PUBLIC_EN_URL || 'https://www.guilhermeguain.com',
+      defaultLocale: 'en-US',
+      locales: ['en-US']
+    },
+    {
+      domain: process.env.NEXT_PUBLIC_PT_URL || 'https://www.guilhermeguain.com.br',
+      defaultLocale: 'pt-BR',
+      locales: ['pt-BR']
+    }
+  ]
 };
