9744914da57d7d5b8d9edea9040fdfcb83d357e8
Guilherme Guain <guilherme.guain@gmail.com>
2022-11-12 12:09:48 -0300

    test

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   i18n: {
     locales: ['en-US', 'pt-BR'],
     defaultLocale: 'en-US',
+    localeDetection: false,
     domains: [
       {
         domain: process.env.NEXT_PUBLIC_EN_URL,
