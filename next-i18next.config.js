48d9fdb95031e9984891a8f90e65cc7514701c28
Guilherme Guain <guilherme.guain@gmail.com>
2022-11-12 17:39:26 -0300

    test

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,20 +1,23 @@
-import { resolve } from 'path';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
-export const i18n = {
-  locales: ['en-US', 'pt-BR'],
-  defaultLocale: 'en-US',
-  localeDetection: false,
-  localePath: resolve('./public/locales'),
-  domains: [
-    {
-      domain: process.env.NEXT_PUBLIC_EN_URL || 'https://www.guilhermeguain.com',
-      defaultLocale: 'en-US',
-      locales: ['en-US']
-    },
-    {
-      domain: process.env.NEXT_PUBLIC_PT_URL || 'https://www.guilhermeguain.com.br',
-      defaultLocale: 'pt-BR',
-      locales: ['pt-BR']
-    }
-  ]
+module.exports = {
+  i18n: {
+    locales: ['en-US', 'pt-BR'],
+    defaultLocale: 'en-US',
+    localeDetection: false,
+    localePath: path.resolve('./public/static/locales'),
+    domains: [
+      {
+        domain: process.env.NEXT_PUBLIC_EN_URL || 'https://www.guilhermeguain.com',
+        defaultLocale: 'en-US',
+        locales: ['en-US']
+      },
+      {
+        domain: process.env.NEXT_PUBLIC_PT_URL || 'https://www.guilhermeguain.com.br',
+        defaultLocale: 'pt-BR',
+        locales: ['pt-BR']
+      }
+    ]
+  }
 };
