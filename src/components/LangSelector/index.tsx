36ccc8785c09e1ffa201f5774e34e62225ad7d18
Guilherme Guain <guilherme.guain@gmail.com>
2022-12-06 20:23:18 -0300

    updated lang selector

--- src/components/LangSelector/index.tsx
+++ src/components/LangSelector/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Select } from '@chakra-ui/react';
+import { Box, Select } from '@chakra-ui/react';
 
 import { useLangSelector } from './hooks/useLangSelector';
 
@@ -7,21 +7,22 @@ export const LangSelector = ({ ...rest }) => {
   const { locale, handleLangChange } = useLangSelector();
 
   return (
-    <Select
-      size="sm"
-      colorScheme="primary"
-      variant="unstyled"
-      color="gray.300"
-      borderRadius="3xl"
-      defaultValue={locale}
-      onChange={(event) => {
-        handleLangChange(event.currentTarget.value);
-      }}
-      sx={{ option: { color: 'gray.800' } }}
-      {...rest}
-    >
-      <option value="en-US">English</option>
-      <option value="pt-BR">Portuguese</option>
-    </Select>
+    <Box border="1px" py={1} pl={4} pr={1} borderRadius="3xl" borderColor="gray.600">
+      <Select
+        size="xs"
+        colorScheme="primary"
+        variant="unstyled"
+        color="gray.300"
+        defaultValue={locale}
+        onChange={(event) => {
+          handleLangChange(event.currentTarget.value);
+        }}
+        sx={{ cursor: 'pointer', option: { color: 'gray.800' } }}
+        {...rest}
+      >
+        <option value="en-US">English</option>
+        <option value="pt-BR">Portuguese</option>
+      </Select>
+    </Box>
   );
 };
