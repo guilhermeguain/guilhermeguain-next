3fb15fe768491e8a12ba4e5983fe37c854c70225
Guilherme Guain <guilherme.guain@gmail.com>
2023-02-21 07:14:14 -0300

    updated dependencies + localepath fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     locales: ['en-US', 'pt-BR'],
     defaultLocale: 'en-US',
     localeDetection: false,
-    localePath: path.resolve('./public/locales'),
+    
     domains: [
       {
         domain: process.env.NEXT_PUBLIC_EN_URL,
@@ -19,5 +19,6 @@ module.exports = {
         locales: ['pt-BR']
       }
     ]
-  }
+  },
+  localePath: path.resolve('./public/locales'),
 };
