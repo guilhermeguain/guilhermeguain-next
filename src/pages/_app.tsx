a84878d8656a5208249e0189a2486829e910a7ba
Guilherme Guain <guilherme.guain@gmail.com>
2023-02-21 06:59:27 -0300

    added logrocket tool

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import TagManager from 'react-gtm-module';
 import { appWithTranslation } from 'next-i18next';
 import { Montserrat } from '@next/font/google';
+import LogRocket from 'logrocket';
 
 import { AppContext } from '@contexts/App';
 
@@ -18,6 +19,7 @@ const fontFamily = Montserrat({
 function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     TagManager.initialize({ gtmId: 'GTM-MFKHPQ4' });
+    LogRocket.init('hsvp97/guilherme-guain');
   }, []);
 
   return (
