3cd55f4f0e02fcd429dac0fc7b39e970716249b0
Guilherme Guain <guilherme.guain@gmail.com>
2023-02-22 09:55:33 -0300

    more tests

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Container } from './styles.css';
 
 export const Button = () => (
-  <div className={Container}>
-    <button>Label</button>
-    <button>Label</button>
-  </div>
+  <button
+    className={Container({
+      color: 'accent',
+    })}
+  >
+    Label
+  </button>
 );
--- src/components/Button/styles.css.ts
+++ src/components/Button/styles.css.ts
@@ -1,16 +1,43 @@
-import { atoms } from '@styles/sprinkles.css';
+import { recipe } from '@vanilla-extract/recipes';
 
-export const Container = atoms({
-  display: 'flex',
-  marginY: {
-    mobile: 'small',
-    tablet: 'medium',
-    desktop: 'large',
+export const Container = recipe({
+  base: {
+    margin: 12,
+    borderRadius: 6,
+    padding: '12px 24px',
   },
-  paddingY: 'medium',
-  paddingX: 'small',
-  flexDirection: {
-    mobile: 'column',
-    desktop: 'row',
+  variants: {
+    color: {
+      neutral: {
+        backgroundColor: 'whitesmoke',
+      },
+      accent: {
+        backgroundColor: 'blueviolet',
+      },
+    },
+    size: {
+      small: { padding: 12 },
+      medium: { padding: 16 },
+    },
+    rounded: {
+      true: { borderRadius: 999 },
+    },
   },
 });
+
+// import { atoms } from '@styles/sprinkles.css';
+
+// export const Container = atoms({
+//   display: 'flex',
+//   marginY: {
+//     mobile: 'small',
+//     tablet: 'medium',
+//     desktop: 'large',
+//   },
+//   paddingY: 'medium',
+//   paddingX: 'small',
+//   flexDirection: {
+//     mobile: 'column',
+//     desktop: 'row',
+//   },
+// });
