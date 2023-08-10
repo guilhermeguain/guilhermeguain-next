0972167c236cf394500550a8c9e6a9dbe1925144
Guilherme Guain <guilherme.guain@gmail.com>
2023-08-09 21:51:29 -0300

    noindex adjust

--- next.config.js
+++ next.config.js
@@ -9,6 +9,22 @@ const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    const headers = [];
+    
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
   i18n
 };
 
