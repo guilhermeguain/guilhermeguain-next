6e2db09a5f374c8da70c8cb9a88afa723d92dfdb
Guilherme Guain <guilherme.guain@gmail.com>
2023-08-09 22:20:07 -0300

    test

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'www.guilhermeguain.com.br',
   generateRobotsTxt: true,
   exclude: ['/server-sitemap-index.xml', '/en-US', '/pt-BR'],
-  generateIndexSitemap: false,
+  // generateIndexSitemap: false,
   alternateRefs: [
     { href: 'https://www.guilhermeguain.com', hreflang: 'en-US' },
     { href: 'https://www.guilhermeguain.com.br', hreflang: 'pt-BR' },
