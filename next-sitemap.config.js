9b2ec1b4e22daa8644648f00ea77b49b934f0ad6
Guilherme Guain <guilherme.guain@gmail.com>
2023-08-09 22:11:21 -0300

    quick adjust

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.guilhermeguain.com.br',
+  siteUrl: process.env.SITE_URL || 'www.guilhermeguain.com.br',
   generateRobotsTxt: true,
   exclude: ['/server-sitemap-index.xml', '/en-US', '/pt-BR'],
   generateIndexSitemap: false,
