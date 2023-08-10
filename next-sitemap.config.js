1d815041bdf572932efec9f11975324e55aa7aae
Guilherme Guain <guilherme.guain@gmail.com>
2023-08-09 22:50:39 -0300

    updated robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'www.guilhermeguain.com.br',
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   exclude: ['/server-sitemap-index.xml', '/en-US', '/pt-BR'],
   generateIndexSitemap: false,
-  // alternateRefs: [
-  //   { href: 'https://www.guilhermeguain.com', hreflang: 'en-US' },
-  //   { href: 'https://www.guilhermeguain.com.br', hreflang: 'pt-BR' },
-  // ],
 };
